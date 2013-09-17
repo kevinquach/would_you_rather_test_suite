4e2fd023fb9a879e92b6cbc00c8895af378f5b05
Jason Zopf <jasonzopf@gmail.com>
2013-09-17 11:44:00 -0400

    remove actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,14 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   # GET /questions
-  # GET /questions.json
   def index
     @most_active = Question.most_active
     @questions = Question.all
   end
 
   # GET /questions/1
-  # GET /questions/1.json
   def show
+    @question = Question.find(params[:id])
   end
 
   # GET /questions/new
@@ -20,12 +18,7 @@ class QuestionsController < ApplicationController
     @question.options.build
   end
 
-  # GET /questions/1/edit
-  def edit
-  end
-
   # POST /questions
-  # POST /questions.json
   def create
     @question = Question.new(question_params)
 
@@ -36,31 +29,10 @@ class QuestionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /questions/1
-  # PATCH/PUT /questions/1.json
-  def update
-    if @question.update(question_params)
-      redirect_to @question, notice: 'Question was successfully updated.'
-    else
-      render action: 'edit'
-    end
-  end
-
-  # DELETE /questions/1
-  # DELETE /questions/1.json
-  def destroy
-    @question.destroy
-    redirect_to questions_url
-  end
-
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_question
-    @question = Question.find(params[:id])
-  end
-
   # Never trust parameters from the scary internet, only allow the white list through.
   def question_params
     params.require(:question).permit(:description, options_attributes: [:id, :question_id, :content])
   end
 end
+
